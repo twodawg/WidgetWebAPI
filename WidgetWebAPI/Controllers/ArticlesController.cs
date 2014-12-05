82b1bb4b646edbc2130af46a7d2e8c1ee84837bd
twodawg <mtwohey2@yahoo.com>
2014-12-04 22:14:58 -0800

    Initial Commit

---
+++ WidgetWebAPI/Controllers/ArticlesController.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using WidgetWebAPI.Models;
+
+namespace WidgetWebAPI.Controllers
+{
+    public class ArticlesController : ApiController
+    {
+        private WidgetModel db = new WidgetModel();
+
+        // GET: api/Articles
+        public IQueryable<Article> GetArticles()
+        {
+            return db.Articles;
+        }
+
+        // GET: api/Articles/5
+        [ResponseType(typeof(Article))]
+        public IHttpActionResult GetArticle(int id)
+        {
+            Article article = db.Articles.Find(id);
+            if (article == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(article);
+        }
+
+        // PUT: api/Articles/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutArticle(int id, Article article)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != article.ArticleID)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(article).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ArticleExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Articles
+        [ResponseType(typeof(Article))]
+        public IHttpActionResult PostArticle(Article article)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Articles.Add(article);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = article.ArticleID }, article);
+        }
+
+        // DELETE: api/Articles/5
+        [ResponseType(typeof(Article))]
+        public IHttpActionResult DeleteArticle(int id)
+        {
+            Article article = db.Articles.Find(id);
+            if (article == null)
+            {
+                return NotFound();
+            }
+
+            db.Articles.Remove(article);
+            db.SaveChanges();
+
+            return Ok(article);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool ArticleExists(int id)
+        {
+            return db.Articles.Count(e => e.ArticleID == id) > 0;
+        }
+    }
+}
---
+++ WidgetWebAPI/Controllers/EmailsController.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using WidgetWebAPI.Models;
+
+namespace WidgetWebAPI.Controllers
+{
+    public class EmailsController : ApiController
+    {
+        private WidgetModel db = new WidgetModel();
+
+        // GET: api/Emails
+        public IQueryable<Email> GetEmails()
+        {
+            return db.Emails;
+        }
+
+        // GET: api/Emails/5
+        [ResponseType(typeof(Email))]
+        public IHttpActionResult GetEmail(int id)
+        {
+            Email email = db.Emails.Find(id);
+            if (email == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(email);
+        }
+
+        // PUT: api/Emails/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutEmail(int id, Email email)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != email.ID)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(email).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!EmailExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Emails
+        [ResponseType(typeof(Email))]
+        public IHttpActionResult PostEmail(Email email)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Emails.Add(email);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = email.ID }, email);
+        }
+
+        // DELETE: api/Emails/5
+        [ResponseType(typeof(Email))]
+        public IHttpActionResult DeleteEmail(int id)
+        {
+            Email email = db.Emails.Find(id);
+            if (email == null)
+            {
+                return NotFound();
+            }
+
+            db.Emails.Remove(email);
+            db.SaveChanges();
+
+            return Ok(email);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool EmailExists(int id)
+        {
+            return db.Emails.Count(e => e.ID == id) > 0;
+        }
+    }
+}
